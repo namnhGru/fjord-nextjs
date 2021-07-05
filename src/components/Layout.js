f44080698a2c62211a896aef6de900e8fffa1f8a
namnh <namnh@namnhs-MacBook-Pro.local>
2021-07-05 15:35:25 +0700

    change font

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -67,8 +67,9 @@ export default class Body extends React.Component {
                     <meta name="description" content={seoDescription} />
                     {!_.isEmpty(seoRobots) && <meta name="robots" content={seoRobots} />}
                     {seoExtra}
-                    <link rel="preconnect" href="https://fonts.gstatic.com" />
-                    <link href="https://fonts.googleapis.com/css2?family=PT+Serif:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
+                    <link rel="preconnect" href="https://fonts.googleapis.com"/> 
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/> 
+                    <link href="https://fonts.googleapis.com/css2?family=STIX+Two+Math&display=swap" rel="stylesheet"></link>
                     {favicon && <link rel="icon" href={withPrefix(favicon)} />}
                     <body className={classNames(`palette-${palette}`)} />
                 </Helmet>
